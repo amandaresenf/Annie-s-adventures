d4590084516822e012fb0e0aa4497f13b9dd603f
amandaresenf <amandaresenf@gmail.com>
2022-04-30 17:13:11 -0300

    abertura e créditos - TimerCena

---
+++ Annie's Adventures/Assets/Scripts/TimerCena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerCena : MonoBehaviour {
+
+    //public string NomeDaCena = "cena";
+    public float tempoParaCarregar = 60;
+
+    float cronometro = 0;
+
+   void Update () {
+        cronometro += Time.deltaTime;
+        if(cronometro > tempoParaCarregar){
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+   }
+}
